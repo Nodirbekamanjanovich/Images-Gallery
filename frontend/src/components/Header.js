3f6bfb276818879993bafc573dde0035d2cd5908
Nodirbek Khabibjonov <habibjonovn@mail.ru>
2022-12-12 23:00:53 +0700

    controlled search component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+
+const navbarStyle = {
+  backgroundColor : "lightblue"
+};
+
+const Header = ({ title }) => {
+  ///const Header = (props) => {
+  ///const {title} = props;
+  return (
+    <Navbar expand="lg" variant="light" style={navbarStyle}>
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
