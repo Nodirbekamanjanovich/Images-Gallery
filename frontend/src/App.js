7bd13387be989b8ea6e5993a8db78d4754d73935
Nodirbek Khabibjonov <habibjonovn@mail.ru>
2022-12-21 13:25:48 +0700

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
@@ -9,28 +11,40 @@ const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
 
-  console.log(images); /// ... and we get actual data
-
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word);
+    ///console.log(word);
     setWord('');
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
     ) ///we have used fetch function that returns Promise. And Promise could be either resolved or rejected
       .then((res) => res.json()) ///response /// and thats what we have actually do when Promise is resolved ...
       .then((data) => {
-        setImages([data, ...images]);
+        ///console.log(data) /// ... and we get actual data
+        setImages([{ ...data, title: word }, ...images]);
       })
       .catch((err) => {
         console.log(err); ///error /// If the Promise is rejected we could print err to the console log and do some other actions afterwards
       });
   };
 
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  };
+
   return (
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
