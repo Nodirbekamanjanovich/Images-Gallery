316b3938903afe5e06b322241810db6a3ecb2573
Nodirbek Khabibjonov <habibjonovn@mail.ru>
2023-01-02 23:21:13 +0700

    Changed api in the frontend and enabled CORS in api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import ImageCard from './components/ImageCard';
 import Welcome from './components/Welcome';
 import { Container, Row, Col } from 'react-bootstrap';
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050';
 
 const App = () => {
   const [word, setWord] = useState('');
@@ -16,9 +16,7 @@ const App = () => {
     e.preventDefault();
     ///console.log(word);
     setWord('');
-    fetch(
-      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
-    ) ///we have used fetch function that returns Promise. And Promise could be either resolved or rejected
+    fetch(`${API_URL}/new-image?query=${word}`) ///we have used fetch function that returns Promise. And Promise could be either resolved or rejected
       .then((res) => res.json()) ///response /// and thats what we have actually do when Promise is resolved ...
       .then((data) => {
         ///console.log(data) /// ... and we get actual data
